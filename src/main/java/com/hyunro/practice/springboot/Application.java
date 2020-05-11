0a1d6ea5afcb0a13e01888bcb66a183e5503895f
hyunro19 <hyunro19@gmail.com>
2020-05-11 11:21:28 +0900

    200511 | CI test commit

--- src/main/java/com/hyunro/practice/springboot/Application.java
+++ src/main/java/com/hyunro/practice/springboot/Application.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("test console");
         SpringApplication.run(Application.class, args);
     }
 }
